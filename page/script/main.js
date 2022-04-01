0e7868c4b536811e31366e811788679b62c39eb3
DESKTOP-KAC91CH <60472956+JYATMorz@users.noreply.github.com>
2022-04-01 15:48:14 +0800

    template for map selection (no zoom)

--- page/script/main.js
+++ page/script/main.js
@@ -3,11 +3,10 @@
  * https://apps.gis.ucla.edu/geodata/dataset/los-angeles-county-neighborhoods
  */
 
-const regionObj = {
+const regionNameObj = {
     China: ["Shenzhen", "Shanghai"],
     USA: ["Atlanta", "Los Angeles"]
 };
-console.log(Object.keys(regionObj));
 
 // enter code to define margin and dimensions for svg
 const margin = { top: 20, right: 20, bottom: 20, left: 20 };
@@ -15,25 +14,7 @@ const width = 800 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
 
 // define any other global variables 
-const pathToJSON = "..\data\la-county-neighborhoods-v6.geojson";
-
-// import csv/json data
-let region = {}, crimeData = [];
-// const csvPromise = new Promise((resolve, reject) => {});
-const jsonPromise = new Promise((resolve, reject) => {
-    d3.json(pathToJSON)
-        .then(temp => resolve(temp))
-        .catch(err => reject("JSON\n" + err));
-});
-Promise.all([jsonPromise]).then(value => {
-    region = value[0];
-}).catch(err => {
-    error = "Error in " + err;
-}).then(() => ready(err, region, crimeData));
-
-function ready(err, region, crimeData) {
-
-}
+const pathToJSON = "data/la-county-neighborhoods-v6.geojson";
 
 // enter code to create color scale
 const colorHue = ["#fee5d9", "#fcae91", "#fb6a4a", "#cb181d"];
@@ -78,3 +59,87 @@ const svg = d3.select("#svg-div").append("svg").attr("id", "svgGeoMap")
     .attr("height", height + margin.top + margin.bottom);
 const gRegion = svg.append("g").attr("id", "svgGeoRegion")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// enter code to define projection and path required for Choropleth
+const projection = d3.geoAlbers();
+const path = d3.geoPath(projection);
+
+// import csv/json data
+let error = "", region = {}, crimeData = [];
+// const csvPromise = new Promise((resolve, reject) => {});
+const jsonPromise = new Promise((resolve, reject) => {
+    d3.json(pathToJSON)
+        .then(temp => resolve(temp))
+        .catch(err => reject("JSON\n" + err));
+});
+Promise.all([jsonPromise]).then(value => {
+    region = value[0];
+}).catch(err => {
+    error = "Error in " + err;
+}).then(() => ready(error, region, crimeData, regionNameObj));
+
+function ready(error, region, crimeData, regionNameObj) {
+    if (error.length != 0) {
+        console.log(error);
+        alert(error);
+    } else if (Object.keys(region).length == 0 || Object.keys(regionNameObj).length == 0) {
+        console.log("Error:\nNo error in Promise.\nBut some data is empty!");
+        alert("Error:\nNo error in Promise.\nBut some data is empty!")
+    } else {
+        // enter code to extract all datas from crimeData
+
+        // enter code to append the region options to the dropdown
+        regionDropdown.selectAll("optgroup")
+            .data(Object.keys(regionNameObj)).enter()
+            .append("optgroup")
+            .attr("class", "region-select")
+            .attr("label", d => d);
+        for (let country in regionNameObj) {
+            if (Object.hasOwnProperty.call(regionNameObj, country)) {
+                let regionArr = regionNameObj[country];
+                regionDropdown.selectAll("optgroup")
+                    .filter((d, i) => d == country)
+                    .selectAll("option")
+                    .data(regionArr).enter()
+                    .append("option")
+                    .attr("class", "region-select")
+                    .attr("value", d => d)
+                    .text(d => d);
+            }
+        }
+        // event listener for the dropdown. 
+        // Update geoMap when selection changes.
+        regionDropdown.on("change", function () {
+            selectedRegion = this.options[this.selectedIndex].value;
+            createMap(region, crimeData, selectedRegion);
+        });
+    }
+}
+
+function createMap(region, crimeData, selectedRegion) {
+    gRegion.selectAll("path").remove();
+    gRegion.selectAll("text").remove();
+    if (selectedRegion != "Los Angeles") {
+        gRegion.append("text")
+            .attr("class", "notification-text")
+            .attr("x", 0.5 * width)
+            .attr("y", 0.5 * height)
+            .text("No Data");
+    } else {
+        projection.fitSize([width, height], region);
+        gRegion.selectAll("path")
+            .data(region.features).enter()
+            .append("path")
+            .attr("d", path)
+            .attr("stroke-width", "0.2px")
+            .attr("stroke", "white")
+            .attr("fill", "gray")
+            .on("mouseover", function (d, i) {
+                d3.select(this).raise()
+                    .attr("fill", "lightgray")
+            }).on("mouseout", function (d, i) {
+                d3.select(this).raise()
+                    .attr("fill", "gray")
+            });
+    }
+}
